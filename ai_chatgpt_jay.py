3121f2957108af5294306928fe11689aa4a03588
Jayasree Varadarajan <85859476+Jayasreev13@users.noreply.github.com>
2023-07-29 19:16:06 +0100

    Update ai_chatgpt_jay.py

--- ai_chatgpt_jay.py
+++ ai_chatgpt_jay.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 st.title("ChatGPT- AI Chat Bot")
 
-openai.api_key ="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"
+openai.api_key ="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"
 
 if "openai_model" not in st.session_state:
     st.session_state["openai_model"] = "gpt-3.5-turbo"
