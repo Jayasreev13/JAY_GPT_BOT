f32683f75ddc8ae42fa1e79485f01f69bb145455
Jayasree Varadarajan <85859476+Jayasreev13@users.noreply.github.com>
2023-07-29 16:32:14 +0100

    Update app.py

--- app.py
+++ app.py
@@ -48,6 +48,7 @@ def chatbot():
         for message in messages:
             st.write(message["content"])
             st.audio(text_to_speech(system_response), format="audio/wav")
+            
 if __name__ == '__chatbot__':
     chatbot()                     
 
