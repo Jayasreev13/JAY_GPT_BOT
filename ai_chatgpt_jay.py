ff239203d9d6b79df6a31a1a61ba2eb86dba3237
Jayasree Varadarajan <85859476+Jayasreev13@users.noreply.github.com>
2023-07-29 19:01:53 +0100

    Update ai_chatgpt_jay.py

--- ai_chatgpt_jay.py
+++ ai_chatgpt_jay.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 st.title("ChatGPT-like clone")
 
-openai.api_key = st.secrets["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"]
+openai.api_key = "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"
 
 if "openai_model" not in st.session_state:
     st.session_state["openai_model"] = "gpt-3.5-turbo"
