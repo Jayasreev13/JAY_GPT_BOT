5f10bd1656957ece9fbf5857c955e6d7a8c61e38
Jayasree Varadarajan <85859476+Jayasreev13@users.noreply.github.com>
2023-07-30 22:33:19 +0100

    Update aibot.py

--- aibot.py
+++ aibot.py
@@ -2,19 +2,17 @@ import streamlit as st
 from gtts import gTTS  
 from io import BytesIO  
 
-st.title("Jay's AI Voice Bot")
+st.title("Jay's AI Speech Synthesizer")
 st.video('jay_ai_voicebot.mp4')
 
-
-def text_to_speech(response):
-    """
-    Converts text to an audio file using gTTS and returns the audio file as binary data
-    """
+"""Converts text to an audio file using gTTS and returns the audio file as binary data"""
+def text_to_speech(response):    
     audio_bytes = BytesIO()
     tts = gTTS(text=response, lang="en")
     tts.write_to_fp(audio_bytes)
     audio_bytes.seek(0)
     return audio_bytes.read()
+    
 # Initialize chat history
 if "messages" not in st.session_state:
     st.session_state.messages = []
@@ -22,17 +20,19 @@ if "messages" not in st.session_state:
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
+        
 # React to user input
 if prompt := st.chat_input("What is up?"):
     # Display user message in chat message container
     st.chat_message("user").markdown(prompt)
     # Add user message to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
-    #response = f"Echo: {prompt}"
     response = f"{prompt}"
+    
     # Display assistant response in chat message container
     with st.chat_message("assistant"):
         st.markdown(response)
         st.audio(text_to_speech(response), format="audio/wav")
+        
     # Add assistant response to chat history
     st.session_state.messages.append({"role": "assistant", "content": response})
