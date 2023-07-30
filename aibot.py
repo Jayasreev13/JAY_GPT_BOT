c2ff730696397e9bab0737819696db2146db5daa
Jayasree Varadarajan <85859476+Jayasreev13@users.noreply.github.com>
2023-07-30 22:27:49 +0100

    Update aibot.py

--- aibot.py
+++ aibot.py
@@ -1,10 +1,10 @@
 import streamlit as st
-from gtts import gTTS  # new import
-from io import BytesIO  # new import
-
+from gtts import gTTS  
+from io import BytesIO  
 
 st.title("Jay's AI Voice Bot")
 st.video('jay_ai_voicebot.mp4')
+
 
 def text_to_speech(response):
     """
@@ -15,23 +15,19 @@ def text_to_speech(response):
     tts.write_to_fp(audio_bytes)
     audio_bytes.seek(0)
     return audio_bytes.read()
-
 # Initialize chat history
 if "messages" not in st.session_state:
     st.session_state.messages = []
-
 # Display chat messages from history on app rerun
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
-
 # React to user input
 if prompt := st.chat_input("What is up?"):
     # Display user message in chat message container
     st.chat_message("user").markdown(prompt)
     # Add user message to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
-
     #response = f"Echo: {prompt}"
     response = f"{prompt}"
     # Display assistant response in chat message container
@@ -40,4 +36,3 @@ if prompt := st.chat_input("What is up?"):
         st.audio(text_to_speech(response), format="audio/wav")
     # Add assistant response to chat history
     st.session_state.messages.append({"role": "assistant", "content": response})
- 
