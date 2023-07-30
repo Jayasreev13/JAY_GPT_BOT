1cda87cee383d7ee6a348d3299d5b2053cbf1eaf
Jayasree Varadarajan <85859476+Jayasreev13@users.noreply.github.com>
2023-07-30 20:24:11 +0100

    Update ai_chatgpt_jay.py

--- ai_chatgpt_jay.py
+++ ai_chatgpt_jay.py
@@ -1,8 +1,12 @@
 import openai
 import streamlit as st
 
+# Streamlit Dashboard
+st.markdown("<h1 style='text-align: center; color: blue;'>I am Jay! Your AI VideoBot </h1>", unsafe_allow_html=True)
+st.image("jay_standgif.gif", width = 200)
+st.title("Jay's AI Bot using ChatGPT")
+st.header("Start your Conversation with Jay!")
 
-st.title("ChatGPT-AI Bot")
 
 openai.api_key = "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"
 
