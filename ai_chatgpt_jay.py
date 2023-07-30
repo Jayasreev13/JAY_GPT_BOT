47b92cdf22e525114c48c7bd4007faa204e7371e
Jayasree Varadarajan <85859476+Jayasreev13@users.noreply.github.com>
2023-07-30 20:59:00 +0100

    Update ai_chatgpt_jay.py

--- ai_chatgpt_jay.py
+++ ai_chatgpt_jay.py
@@ -2,12 +2,12 @@ import openai
 import streamlit as st
 # Streamlit Dashboard
 st.markdown("<h1 style='text-align: center; color: blue;'>I am Jay! Your AI VideoBot </h1>", unsafe_allow_html=True)
-#st.image("jay_standgif.gif", width = 500)
+st.video('jay_audio.mp4')
+st.caption('I am Jay! An AI Digital Human created using D-ID for So.evo'
 st.title("Jay's AI Bot using ChatGPT")
 st.header("Start your Conversation with Jay!")
 #video_bytes = video_file.read(jay_audio.mp4)
-st.video('jay_audio.mp4')
-st.caption('I am Jay! An AI Digital Human created using D-ID for So.evo')
+)
 
 openai.api_key = "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"
 
