f62742d2d6aa0acfae3fd873571f63827722c87d
Jayasree Varadarajan <85859476+Jayasreev13@users.noreply.github.com>
2023-07-30 20:57:24 +0100

    Update ai_chatgpt_jay.py

--- ai_chatgpt_jay.py
+++ ai_chatgpt_jay.py
@@ -1,7 +1,5 @@
 import openai
 import streamlit as st
-
-
 # Streamlit Dashboard
 st.markdown("<h1 style='text-align: center; color: blue;'>I am Jay! Your AI VideoBot </h1>", unsafe_allow_html=True)
 #st.image("jay_standgif.gif", width = 500)
@@ -9,25 +7,21 @@ st.title("Jay's AI Bot using ChatGPT")
 st.header("Start your Conversation with Jay!")
 #video_bytes = video_file.read(jay_audio.mp4)
 st.video('jay_audio.mp4')
-st.caption("This Digital human AI Bot is created using D-ID for So.Evo")
+st.caption('I am Jay! An AI Digital Human created using D-ID for So.evo')
 
 openai.api_key = "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"
 
 if "openai_model" not in st.session_state:
     st.session_state["openai_model"] = "gpt-3.5"
-
 if "messages" not in st.session_state:
     st.session_state.messages = []
-
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
-
-if prompt := st.chat_input("What's up ?"):
+if prompt := st.chat_input("What is up?"):
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         st.markdown(prompt)
-
     with st.chat_message("assistant"):
         message_placeholder = st.empty()
         full_response = ""
@@ -43,4 +37,3 @@ if prompt := st.chat_input("What's up ?"):
             message_placeholder.markdown(full_response + "▌")
         message_placeholder.markdown(full_response)
     st.session_state.messages.append({"role": "assistant", "content": full_response})
-    
